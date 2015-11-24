7f15d7750f7a29c64fd06d62df66cf6286bc64fc
Perry Yeh <perry@yeeh.org>
2015-11-24 10:59:58 +0800

    update website

---
+++ e/d/slide_13.js
@@ -0,0 +1,42 @@
+﻿var Show_Style=3;
+var Image_13=new Array();
+var Pics="/e/css/image/banner_3.jpg";
+var Links="";
+var Titles="";
+var Alts="";
+var Apic13=Pics.split('|');
+var ALink13=Links.split('|');
+var ATitle13=Titles.split('|');
+var AAlts13=Alts.split('|');
+var Show_Text=0;
+for(i=0;i<Apic13.length;i++)
+  {
+   Image_13.src = Apic13[i]; 
+  }
+
+
+
+  var FHTML='<div id="js_slide_focus_13" class="slide_focus focus_style3" style="height:295px">';
+  FHTML+='<ul class="inner">';
+  for(var i=0;i<Apic13.length;i++)
+   {
+     if(ALink13.length<(i+1) || ALink13[i]=="")
+      {
+       ALink13[i]="javascript:void(0)";
+      }
+     if(AAlts13.length<(i+1))
+      {
+       AAlts13[i]="";
+      }
+     if(ATitle13.length<(i+1))
+      {
+       ATitle13[i]="";
+      }
+    FHTML+='<li><a href="'+ALink13[i]+'" target="_self" title="'+AAlts13[i]+'"><img src="'+Apic13[i]+'">';
+    FHTML+='<em>'+ATitle13[i]+'</em>';
+    FHTML+='</a></li>';
+   }
+ FHTML+='</ul>';
+ FHTML+='</div>';
+ document.write(FHTML);
+$(function(){Slide_Focus("js_slide_focus_13",0,5,1440,295,true);});
